b8b1644fe817bc66f304eb25d92918e785365246
Sandeep Aggarwal <@smartSandeep1129@gmail.com>
2017-07-08 19:05:05 +0530

    Initial Commit

---
+++ SAIssueTracker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SAIssueTracker
+//
+//  Created by Sandeep Aggarwal on 08/07/17.
+//  Copyright © 2017 Sandeep Aggarwal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
