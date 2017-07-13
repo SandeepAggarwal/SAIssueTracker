535cc1a626676daf0b5a18692db1b307e98124f4
Sandeep Aggarwal <@smartSandeep1129@gmail.com>
2017-07-13 17:59:00 +0530

    pods added

---
+++ Pods/Target Support Files/Pods-SAIssueTrackerTests/Pods-SAIssueTrackerTests-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_SAIssueTrackerTestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_SAIssueTrackerTestsVersionString[];
+
