f428d03f05e52965359368f5bb7345941f3eaddf
Sandeep Aggarwal <@smartSandeep1129@gmail.com>
2017-07-13 16:49:57 +0530

    .gitignore is now working

--- Pods/Target Support Files/Pods-SAIssueTrackerTests/Pods-SAIssueTrackerTests-umbrella.h
+++
@@ -1,8 +0,0 @@
-#ifdef __OBJC__
-#import <UIKit/UIKit.h>
-#endif
-
-
-FOUNDATION_EXPORT double Pods_SAIssueTrackerTestsVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_SAIssueTrackerTestsVersionString[];
-
